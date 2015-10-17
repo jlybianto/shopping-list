e4bc2940ade8317f0095fdc8c292bbb7e3c3c697
jlybianto <jesse.lybianto@gmail.com>
2015-10-17 11:56:25 -0600

    Correct item adding function and add other functionalities

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
 function addListItem() {
     var write = $('#newItem').val();
     var list = $('#itemList');
-    var item = $('<li><input class="checkbox" type="checkbox">
-                <span class="item">' + write + '</span>
-                <button class="delete">x</button></li>')
+    var item = $('<li><input class="checkbox" type="checkbox"><span class="item">' + toTitleCase(write) + '</span><button class="delete">x</button></li>');
     var newItem = $("#newItem");
 
     if (write.length === 0 || write.length > 24) {
@@ -14,6 +16,20 @@ function addListItem() {
     $(newItem).val('');
 }
 
+function removeItem() {
+    $(this).parent().remove();
+}
+
+$(document).on('click', '#resetButton',
+    function() {
+        $('#itemList').empty();
+    }
+);
+
+function tickItem() {
+    $(this).parent().toggleClass('checked-off');
+}
+
 $(function() {
     var add = $('#addItem');
     var newItem = $('#newItem');
@@ -21,6 +37,7 @@ $(function() {
 
     add.on('click', addListItem);
     list.on('click', '.delete', removeItem);
+    list.on('click', '.checkbox', tickItem);
     newItem.on('keypress', function(e) {
         if (e.which == 13) {
             addListItem();
