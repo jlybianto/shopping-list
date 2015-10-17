bd778392f98ec52f357234d8f5a33a5c0f2cabbb
jlybianto <jesse.lybianto@gmail.com>
2015-10-17 10:58:33 -0600

    Rename list and reconfigure JS link

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
-$(document).ready(function() {
-$('#newItem').focus();
-$('#addItem').click(submit);
-$('#newItem').keydown(function(e) {
-	if (e.keyCode == 13) {
-		submit();
-	}
+function addListItem() {
+    var write = $('#newItem').val();
+    var list = $('#itemList');
+    var item = $('<li><input class="checkbox" type="checkbox">
+                <span class="item">' + write + '</span>
+                <button class="delete">x</button></li>')
+    var newItem = $("#newItem");
+
+    if (write.length === 0 || write.length > 24) {
+        return false;
+    }
+
+    list.prepend(item);
+    $(newItem).val('');
+}
+
+$(function() {
+    var add = $('#addItem');
+    var newItem = $('#newItem');
+    var list = $('#itemList');
+
+    add.on('click', addListItem);
+    list.on('click', '.delete', removeItem);
+    newItem.on('keypress', function(e) {
+        if (e.which == 13) {
+            addListItem();
+        }
+    });
 });
